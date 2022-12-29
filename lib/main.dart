34f7031b8e3f5844e83f6a1b64c1d58b08d4fe0c
Gilbert <gssenyonjo@gmail.com>
2022-12-29 15:08:33 +0300

    implemented  favourites list screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Welcome dear",
-        home: Scaffold(
-            appBar: AppBar(title: const Text("Name Generator")),
-            body: const Center(child: RandomWords())));
+    return const MaterialApp(title: "Name Generator", home: RandomWords());
   }
 }
 
@@ -30,34 +26,58 @@ class _RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18);
   final _saved = <WordPair>[];
 
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(
+          title: Text(pair.asPascalCase, style: _biggerFont),
+        );
+      });
+      final divided = tiles.isNotEmpty
+        ? ListTile.divideTiles(tiles: tiles, context: context).toList() : <Widget>[];
+      return Scaffold(
+        appBar: AppBar(title: const Text("Saved Suggestions")),
+        body: ListView(children: divided),
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(20));
-          }
-          final alreadySaved = _saved.contains(_suggestions[index]);
-          return ListTile(
-            title: Text(_suggestions[index].asPascalCase, style: _biggerFont),
-            trailing: Icon(
-                alreadySaved ? Icons.favorite : Icons.favorite_border,
-                color: alreadySaved ? Colors.red : null,
-                semanticLabel: alreadySaved ? "Remove from saved" : "Save"),
-            onTap: () {
-              setState(() {
-                if (alreadySaved) {
-                  _saved.remove(_suggestions[index]);
-                } else {
-                  _saved.add(_suggestions[index]);
-                }
-              });
-            },
-            
-          );
-        });
+    return Scaffold(
+        appBar: AppBar(title: const Text("Name Generator"), actions: [
+          IconButton(
+            onPressed: _pushSaved,
+            icon: const Icon(Icons.list),
+            tooltip: 'Saved suggestions',
+          )
+        ]),
+        body: ListView.builder(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+            itemBuilder: (context, i) {
+              if (i.isOdd) return const Divider();
+              final index = i ~/ 2;
+              if (index >= _suggestions.length) {
+                _suggestions.addAll(generateWordPairs().take(20));
+              }
+              final alreadySaved = _saved.contains(_suggestions[index]);
+              return ListTile(
+                  title: Text(_suggestions[index].asPascalCase,
+                      style: _biggerFont),
+                  trailing: Icon(
+                      alreadySaved ? Icons.favorite : Icons.favorite_border,
+                      color: alreadySaved ? Colors.red : null,
+                      semanticLabel:
+                          alreadySaved ? "Remove from saved" : "Save"),
+                  onTap: () {
+                    setState(() {
+                      if (alreadySaved) {
+                        _saved.remove(_suggestions[index]);
+                      } else {
+                        _saved.add(_suggestions[index]);
+                      }
+                    });
+                  });
+            }));
   }
 }
